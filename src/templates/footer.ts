a1b210ee294ae2151ecf281ffa5981869079d870
Vytenis Butkevicius <vytenis.butkevicius@gmail.com>
2017-04-22 22:49:17 +0300

    Make footer display real year

--- src/templates/footer.ts
+++ src/templates/footer.ts
@@ -2,10 +2,13 @@ import { a } from './elements';
 import { ContainerIds } from './constants';
 
 export function footer(commitsCount: number): string {
+    const currentYear = `${new Date().getFullYear()}`;
+    const originalDate = '2017';
+
     return `
         <footer>
             <div class="footer-inner">
-                <div class="built-by">Built by Vytenis Butkevicius, 2017</div>
+                <div class="built-by">Built by Vytenis Butkevicius, ©  ${currentYear !== originalDate ? `${originalDate} - ` : ''} ${currentYear}</div>
                 <div class="powered-by">Proudly Powered By ${a(ContainerIds.Noop, 'https://github.com/CanisMajorisLT/blog', `${commitsCount} Commits`)}</div>
             </div>
         </footer>
