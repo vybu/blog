232f302635455adcd28c4a92fde6f3e6cfe1ec43
Vytenis Butkevicius <vytenis.butkevicius@gmail.com>
2017-04-29 12:16:37 +0300

    New design / typography core

--- src/templates/header.ts
+++ src/templates/header.ts
@@ -5,7 +5,12 @@ export function header(): string {
     return `
         <header>
             <div class="header-inner">
-                <h1>${a(ContainerIds.App, '/', 'Vytenis Blog')}</h1>
+                <h3>${a(ContainerIds.App, '/', `
+                <div class="lb-unit-container">
+                    <div class="lb-unit"></div>
+                    <div class="lb-unit"></div>
+                    <div class="lb-unit"></div>
+                </div>`)}</h2>
                 <nav>
                     <span class="about">${a(ContainerIds.Noop, 'https://github.com/CanisMajorisLT', 'Github')}</span>                    
                     <span class="about">${a(ContainerIds.App, '/about', 'About')}</span>                    
